63f5255012059d9a1865971756ce8f8c5961e9ac
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-11-19 19:23:28 -0600

    Refactor GameSimonSays component and update prompt messages

--- src/components/GameSimonSays.jsx
+++ src/components/GameSimonSays.jsx
@@ -1,64 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import ExitButton from '../components/ExitButton';
 import './GameSimonSays.css';
 
 function GameSimonSays() {
- 
+  const activeProfile = useSelector((state) => state.profiles.activeProfile); // Access activeProfile from Redux store
+
   const [prompt, setPrompt] = useState('');
   const [userInput, setUserInput] = useState('');
   const [feedback, setFeedback] = useState('');
-  const [correctInfo, setCorrectInfo] = useState({
-    name: 'John Doe',
-    address: '123 Main St',
-    phone: '123-456-7890',
-    school: 'ABC High School',
-    parentsName: 'Jane and John Doe',
-  });
-
 
   const prompts = [
-    'What is your name?',
-    'What is your home address?',
-    'What is your phone number?',
+    'What is your nickname?',
+    'What is your grade?',
     'What is your school name?',
-    'What is your parents\' name?',
+    'Who is your teacher?',
+    'What is your address?',
   ];
-
 
   const generatePrompt = () => {
     const randomIndex = Math.floor(Math.random() * prompts.length);
     setPrompt(prompts[randomIndex]);
   };
 
- 
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
   };
 
-  
   const checkAnswer = () => {
     let correctAnswer = '';
 
     switch (prompt) {
-      case 'What is your name?':
-        correctAnswer = correctInfo.name;
+      case 'What is your nickname?':
+        correctAnswer = activeProfile?.nickname || 'n/a';
         break;
-      case 'What is your home address?':
-        correctAnswer = correctInfo.address;
-        break;
-      case 'What is your phone number?':
-        correctAnswer = correctInfo.phone;
+      case 'What is your grade?':
+        correctAnswer = activeProfile?.gradeClass || 'n/a';
         break;
       case 'What is your school name?':
-        correctAnswer = correctInfo.school;
+        correctAnswer = activeProfile?.schoolName || 'n/a';
         break;
-      case 'What is your parents\' name?':
-        correctAnswer = correctInfo.parentsName;
+      case 'Who is your teacher?':
+        correctAnswer = activeProfile?.schoolTeacher || 'n/a';
+        break;
+      case 'What is your address?':
+        correctAnswer = activeProfile?.address || 'n/a'; // Add the address field here
         break;
       default:
         break;
     }
-
 
     if (userInput.trim().toLowerCase() === correctAnswer.toLowerCase()) {
       setFeedback('Correct!');
@@ -68,7 +58,7 @@ function GameSimonSays() {
   };
 
   useEffect(() => {
-    generatePrompt();  
+    generatePrompt();
   }, []);
 
   return (
